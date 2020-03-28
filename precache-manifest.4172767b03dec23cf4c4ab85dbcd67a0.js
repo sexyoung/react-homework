e3ef513484d7e76d57886f07696ec1aa0dd102d3
sexyoung <Habuche@gmail.com>
2020-03-28 19:39:24 +0000

    Deploying to gh-pages from master 9e68c865074f65944e0862466d102babe27a5c78

---
+++ precache-manifest.4172767b03dec23cf4c4ab85dbcd67a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2f2273dfd6f32de937a4088f264c94",
+    "url": "/react-homework/index.html"
+  },
+  {
+    "revision": "667d8d184f09ab24feb4",
+    "url": "/react-homework/static/css/main.a4316710.chunk.css"
+  },
+  {
+    "revision": "96140ec24a0c3669bc7b",
+    "url": "/react-homework/static/js/2.a1549c8c.chunk.js"
+  },
+  {
+    "revision": "667d8d184f09ab24feb4",
+    "url": "/react-homework/static/js/main.53fed59e.chunk.js"
+  },
+  {
+    "revision": "8a28aa7ee69d0964e693",
+    "url": "/react-homework/static/js/runtime-main.f27fed78.js"
+  }
+]);
